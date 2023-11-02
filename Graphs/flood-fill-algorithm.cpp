2744e67ca6bc77379cee0b9cc413fc1223a9eb15
Saswati1701 <92461669+Saswati1701@users.noreply.github.com>
2023-11-02 20:43:58 +0000

    added notes

--- Graphs/flood-fill-algorithm.cpp
+++ Graphs/flood-fill-algorithm.cpp
@@ -5,6 +5,10 @@ void dfs(vector<vector<int>>& ans, vector<vector<int>>& image, int sr, int sc, i
         for(int i=0; i<4; i++){
             int nrow = sr + delrow[i];
             int ncol = sc + delcol[i];
+
+            //nrow ans ncol stay inside the matrix
+            //if the the color in new position(nrow, ncol) is equal to starting position(sr, sc)
+            //if the color in the new matrix hasn't already changed then run dfs for the (nrow, ncol)
             if(nrow>=0 && nrow<M && ncol>=0 && ncol<N && image[nrow][ncol]==orgColor && ans[nrow][ncol]!=newColor){
                 dfs(ans, image, nrow, ncol, newColor, orgColor, delrow, delcol);
             }
@@ -12,8 +16,11 @@ void dfs(vector<vector<int>>& ans, vector<vector<int>>& image, int sr, int sc, i
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         // Code here 
+        //ALways use a copy of original matrix. Don't tamper with given data
         vector<vector<int>> ans = image;
         int orgColor = ans[sr][sc];
+
+        //the four directions traversed as nodes of the graph
         int delrow[] = {0, -1, 0, 1};
         int delcol[] = {-1, 0, 1, 0};
         
